23e7a746d1094b86db81a34c02ee0cbf88172d9c
kyomei <rafa.jefer@gmail.com>
2018-10-25 11:43:25 -0300

    Pre-configurando as routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('quem-somos', function(){
+    return '<h1>Página quem somos</h1>';
+});
+Route::get('fale-conosco', function(){
+    return '<h1>Página fale-conosco</h1>';
+});
+Route::get('cursos', function(){
+    return '<h1>Página cursos</h1>';
+});
+Route::get('cursos/cursos-livres', function(){
+    return '<h1>Página cursos livres</h1>';
+});
+Route::get('cursos/pacote-de-cursos', function(){
+    return '<h1>Página pacote de cursos</h1>';
+});

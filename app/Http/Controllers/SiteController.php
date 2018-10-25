34827f1d19045bab0281fae3533b53aa864365c0
kyomei <rafa.jefer@gmail.com>
2018-10-25 11:51:14 -0300

    SiteController criado e com os metodos de exibicao de conteudo pre-configurados

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,36 @@
+<?php namespace ieatprof\Http\Controllers;
+
+class SiteController {
+    
+    /* Método responsável pela exibição das informações na pág home */
+    public function home(){
+        
+    }
+    
+    /* Método responsável pela exibição das informações na pág quem-somos */
+    public function quem_somos(){
+        
+    }
+    /* Método responsável pela exibição das informações na pág fale-conosco */
+    public function fale_conosco(){
+    
+        
+    }
+    /* Método responsável pela exibição das informações na pág cursos */
+    public function cursos(){
+        
+    }
+    
+    /* Método responsável pela exibição das informações na pág cursos-livres */
+    public function cursos_livres(){
+        
+    }
+    
+    /* Método responsável pela exibição das informações na pág pacote-de-cursos */
+    public function pacotes_de_cursos(){
+        
+    }
+}
+
+
+

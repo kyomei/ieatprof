d9a29a30f9f64c27db805247f3cc8c13400d3032
Rafael Jeferson <rafa.jefer@gmail.com>
2018-10-29 14:31:38 -0300

    Add boostrap 4.1.3 no projeto via CDN

--- resources/views/listagem.php
+++ resources/views/listagem.php
@@ -3,14 +3,27 @@
     <head>
         <meta charset="UTF-8">
         <title>Cursos Livres</title>
-        <link rel="stylesheet" href="/css/app.css" />
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     </head>
     <body>
         <h1>Listagem de cursos livres</h1>
-        <ul>
+        <table class="table">            
             <?php foreach ($modulos as $modulo) : ?>
-                <li><?= $modulo->nome_modulo ?></li>
+                <tr>
+                    <td><?= $modulo->nome_modulo ?></td>
+                </tr>
             <?php endforeach ?>
-        </ul>
+        </table>
+
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <!-- Popper JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> 
     </body>
 </html>
--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -3,11 +3,22 @@
     <head>
         <meta charset="UTF-8">
         <title>Instituto Educacional Alto Tietê</title>
-        <link rel="stylesheet" href="/css/app.css" />
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     </head>
     <body>
         <div class="container">
             @yield('conteudo')
         </div>
+
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <!-- Popper JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> 
     </body>
 </html>
